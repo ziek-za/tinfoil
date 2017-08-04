8c81b4d48b1a953c347f5a1c2c8b1999f55c0719
Darryn Papathanasiou <darryn.papathanasiou@darryn-papathanasiou-154.local>
2017-08-04 23:11:57 +0200

    Reworked scene and updated cull.

--- Assets/scripts/cull_rotation.cs
+++ Assets/scripts/cull_rotation.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cull_rotation : MonoBehaviour {
+	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("TinfoilMan");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 direction = (this.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition)).normalized;
+		Vector3 direction = (this.transform.position - player.transform.position).normalized;
 		this.transform.right = new Vector3(direction.x, direction.y);
 	}
 }
