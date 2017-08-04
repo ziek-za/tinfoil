060f40953df396f204d9a4206d87e7f28911234c
Darryn Papathanasiou <darryn.papathanasiou@darryn-papathanasiou-154.local>
2017-08-05 00:01:38 +0200

    Added scaling to black cull to get bigger when closer and visa versa lalala.

--- Assets/scripts/cull_rotation.cs
+++ Assets/scripts/cull_rotation.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class cull_rotation : MonoBehaviour {
 	private GameObject player;
+	private int max_distance;
+	private Transform child;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("TinfoilMan");
+		floor_generation floor = GameObject.Find("floor").GetComponent<floor_generation>();
+		max_distance = (floor.width / 2);
+		child = this.transform.GetChild (0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Update direction
 		Vector3 direction = (this.transform.position - player.transform.position).normalized;
 		this.transform.right = new Vector3(direction.x, direction.y);
+		// Update scale based on distance away
+		float distance_from_center = Vector2.Distance(this.transform.position, player.transform.position);
+		float scale_ratio = max_distance / distance_from_center;
+		child.localScale = new Vector3 (scale_ratio, 2, 1);
 	}
 }
