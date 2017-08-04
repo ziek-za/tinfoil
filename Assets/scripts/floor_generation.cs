e8b53d9862d7710de5b3cf4196430be6f5821381
Darryn Papathanasiou <darryn.papathanasiou@darryn-papathanasiou-154.local>
2017-08-04 21:03:00 +0200

    Added dynamic generation of flooring using C# and headers from Zuckerbergs personal collection of libraries.

---
+++ Assets/scripts/floor_generation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor_generation : MonoBehaviour {
+	public GameObject floor_prefab;
+
+	public int width;
+	public int height;
+
+	// Use this for initialization
+	void Start () {
+		// Generate floor tiling
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject.Instantiate (
+					floor_prefab,
+					new Vector3(x, y),
+					Quaternion.identity,
+					this.transform
+				);
+			}
+		}
+		// Center floor
+		Camera.main.transform.position = new Vector3 (width / 2, height / 2, -10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
